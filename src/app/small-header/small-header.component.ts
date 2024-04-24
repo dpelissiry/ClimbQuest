b03109a2066e09b6cfe0c6a2be1b186def36000f
Dylan Elissiry <dpelissiry@gmail.com>
2024-04-24 15:33:14 -0700

    small-header

---
+++ src/app/small-header/small-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-header',
+  templateUrl: './small-header.component.html',
+  styleUrl: './small-header.component.scss'
+})
+export class SmallHeaderComponent {
+  title: string = 'Climb Quest';
+}
