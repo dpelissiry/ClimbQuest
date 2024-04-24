9d02db552452d72227413a30bea202a5a43b9ac6
Dylan Elissiry <dpelissiry@gmail.com>
2024-04-24 15:33:57 -0700

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponenents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { ResultComponent } from './result/result.component';
@@ -11,6 +11,11 @@ import { ResultBlockComponent } from './result-block/result-block.component';
 import { ErrorBlockComponent } from './error-block/error-block.component';
 import { FooterComponent } from './footer/footer.component';
 import { SortComponent } from './sort/sort.component';
+import { PopularSearchComponent } from './popular-search/popular-search.component';
+import { ResultPageComponent } from './result-page/result-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SmallHeaderComponent } from './small-header/small-header.component';
+import { PopularSearchBlockComponent } from './popular-search-block/popular-search-block.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +27,11 @@ import { SortComponent } from './sort/sort.component';
     ErrorBlockComponent,
     FooterComponent,
     SortComponent,
+    PopularSearchComponent,
+    ResultPageComponent,
+    HomePageComponent,
+    SmallHeaderComponent,
+    PopularSearchBlockComponent,
   ],
   imports: [
     BrowserModule,
