239474596dda040608347871a8753a7194bcd1fc
Dylan <dpelissiry@gmail.com>
2024-09-30 11:16:37 -0700

    text change

--- src/app/result-block/result-block.component.ts
+++ src/app/result-block/result-block.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { SafePipe } from 'safe-pipe';
 
 @Component({
   selector: 'app-result-block',
   templateUrl: './result-block.component.html',
-  styleUrl: './result-block.component.scss'
+  styleUrl: './result-block.component.scss',
 })
 export class ResultBlockComponent {
   @Input() data: any; // decorate the property with @Input()
   public hover: boolean = false;
-  constructor() { }
+  constructor() {}
   // ngOnChanges(changes: SimpleChanges) {
   //   if (changes['data'] && this.data && this.data['location']) {
   //     this.data['location'] = this.data['location'].split(" >").slice(1,3)
@@ -19,6 +20,10 @@ export class ResultBlockComponent {
   //   }
   
   // }
+
+  showVideo(){
+    if (this.data['youtube'] != null) {this.hover = true} 
+  }
 
   navigate(){
     window.open(this.data.URL, '_blank');
